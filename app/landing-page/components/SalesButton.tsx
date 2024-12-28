013e48f73d865554a7f0e4e01d029d21bfedc8d9
Leorev01 <revrennaleo@gmail.com>
2024-12-28 13:18:00 +0000

    Adds landing page directory

--- app/landing-page/components/SalesButton.tsx
+++ app/landing-page/components/SalesButton.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 const SalesButton = () => {
   return (
-    <Link href='schedule' className='bg-[#EF233C] text-white font-bold text-center text-2xl p-4 mt-5 rounded-lg flex flex-col items-center transition-transform transform animate-bounce'>
+    <Link href='landing-page/schedule' className='bg-[#EF233C] text-white font-bold text-center text-2xl p-4 mt-5 rounded-lg flex flex-col items-center transition-transform transform animate-bounce'>
           SCHEDULE A FREE CONSULTATION
           <span className='text-lg font-normal'>+ 20% OFF New Years Sale</span>
     </Link>
